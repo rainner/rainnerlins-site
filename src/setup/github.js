ca8b4447f6881addd748d588713323d72a20d9f6
Rainner Lins <rainnerlins@gmail.com>
2018-02-18 06:59:08 -0500

    Small changes to github setup file.

--- src/setup/github.js
+++ src/setup/github.js
@@ -10,7 +10,7 @@ import DomHelper from '../modules/DomHelper';
 const github_api = 'https://api.github.com/graphql';
 const github_key = '9277a09725196cf91f50a3db79934445547b19a1'; // read-only
 
-// loader
+// init loader
 const loader = Loader( '#projects-loader' );
 loader.show( 'Loading list of projects...' );
 
@@ -56,18 +56,18 @@ const buildSlide = ( repo ) => {
   return `
   <div class="slideshow-item hidden">
     <div class="flex-grid flex-top">
-      <div class="flex-grid-item flex-40 fx-on fx-zoom-in">
+      <div class="flex-grid-item flex-40">
         <a href="${ repo.url }" target="_blank">
-          <img class="thumb" src="public/images/thumb.svg" data-image="${ repo.url }/master/thumb.jpg" width="100%" alt="${ repo.name }" />
+          <img class="fx-on fx-drop-in" src="public/images/default-thumb.svg" data-image="${ repo.url }/master/thumb.jpg" width="100%" alt="${ repo.name }" />
         </a>
       </div>
-      <div class="flex-grid-item flex-60 fx-on fx-fade-in">
-        <h3 class="text-clip">${ repo.name }</h3>
-        <div class="text-small text-grey pad-top">${ repo.description }</div>
-        <div class="text-small text-clip pad-top">
+      <div class="flex-grid-item flex-60">
+        <h3 class="heading fx-on fx-fade-in fx-delay-1">${ repo.name }</h3>
+        <div class="text-small text-grey pad-bottom fx-on fx-fade-in fx-delay-2">${ repo.description }</div>
+        <div class="text-small text-clip pad-bottom fx-on fx-fade-in fx-delay-3">
           <a href="${ repo.url }" target="_blank">${ repo.url }</a>
         </div>
-        <div class="pad-top">
+        <div class="fx-on fx-fade-in fx-delay-4">
           <a class="form-btn bg-secondary-hover icon-ghub iconLeft" href="${ repo.url }" target="_blank">View Repository</a>
         </div>
       </div>
@@ -99,10 +99,9 @@ new Ajax( 'POST', github_api, {
 
     // not looking good
     if ( !response || !response.data || Array.isArray( response.errors ) ) {
-      loader.error( 'Status '+ xhr.status +' : Could not load projects, try again later.' );
-      return;
+      return loader.error( 'Status '+ xhr.status +' : Could not load projects, try again later.' );
     }
-    // assume all good, init
+    // data looks good, init
     loader.hide();
     updateUserData( response.data.user );
     buildSlideshow( response.data.user.pinnedRepositories.edges );
