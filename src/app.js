ebb9f0d60e05601f541400291d3249c1ce892ca5
Rainner Lins <rainnerlins@gmail.com>
2018-03-22 14:20:46 -0400

    Changed main app base script file.

--- src/app.js
+++ src/app.js
@@ -1,47 +1,15 @@
 /**
  * Main app entry file.
  */
-
-// css bundle entry file
 import './scss/app.scss';
-// client side polyfills
 import './modules/Polyfills';
-// setup page elements
 import './setup/github';
-import './setup/services';
-import './setup/contact';
+import './setup/links';
 import './setup/dropdowns';
 import './setup/tooltips';
 import './setup/scroller';
-
-// lazy-load images for some elements
-const updateImages = () => {
-  let attr = 'data-image';
-  let elms = document.querySelectorAll( '['+ attr +']' );
-
-  for ( let i = 0; i < elms.length; ++i ) {
-    let elm  = elms[ i ];
-    let file = elm.getAttribute( attr );
-    let img  = new Image();
-
-    img.addEventListener( 'load', e => {
-      // image tags
-      if ( elm.tagName === 'IMG' ) {
-        // replace old image with new one to trigger css animation once loaded
-        let newImg = elm.cloneNode( false );
-        newImg.src = file;
-        elm.parentNode.replaceChild( newImg, elm );
-      }
-      else {
-        // everything else gets a bg swap
-        elm.style.backgroundImage = 'url( '+ file +' )';
-      }
-    }, false );
-
-    elm.removeAttribute( attr );
-    img.src = file;
-  }
-};
+import './setup/contact';
+import './setup/images';
 
 // on page loaded
 window.addEventListener( 'load', e => {
@@ -51,6 +19,5 @@ window.addEventListener( 'load', e => {
   // show page content
   let container = document.querySelector( '#app' );
   container.style.opacity = 1;
-  // swap images
-  setTimeout( updateImages, 400 );
-});
+  // ...
+}, false );
