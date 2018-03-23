d56bd1e0a9b374b0f0f1387bdbe5a56f2e46317c
Rainner Lins <rainnerlins@gmail.com>
2018-03-22 20:18:17 -0400

    Changed contact form from AJAX to regular POST request.

--- src/setup/contact.js
+++ src/setup/contact.js
@@ -10,37 +10,54 @@ const notify  = new Notify();
 const handler = e => {
   e.preventDefault();
 
-  let host    = window.location.hostname || 'rainnerlins.com';
+  let proto   = window.location.protocol || 'https:';
+  let host    = window.location.host || 'rainnerlins.com';
   let email   = Input( e.target.email ).email().required();
   let message = Input( e.target.message ).min( 20 ).required();
+  let subject = 'Dialogue with '+ email.val() +' on '+ host +'.';
 
+  if ( !e.target.action || e.target.action === '#' ) return;
   if ( !email.valid() )   return notify.warning( email.info() );
   if ( !message.valid() ) return notify.warning( message.info() );
 
-  e.target.setAttribute( 'disabled', 'disabled' );
+  // submit to new URL
+  e.target.setAttribute( 'target', '_self' );
+  e.target._next.value = proto +'//'+ host +'/#mailsent';
+  e.target._subject.value = subject;
+  e.target.submit();
+  e.target.reset();
 
-  new Ajax( 'POST', e.target.action, {
-    data: {
-      _replyto: email.val(),
-      _subject: 'Dialogue with '+ email.val() +' on '+ host +'.',
-      message: message.val(),
-    },
-    success: function( xhr, response ) {
-      notify.success( response.message || 'Message sent, thanks!' );
-      e.target.reset();
-    },
-    error: function( xhr, error ) {
-      notify.error( error );
-    },
-    complete: function( xhr, response ) {
-      e.target.removeAttribute( 'disabled' );
-    },
-  });
+  // submit using AJAX
+  // e.target.setAttribute( 'disabled', 'disabled' );
+  // new Ajax( 'POST', e.target.action, {
+  //   data: {
+  //     _replyto: email.val(),
+  //     _subject: subject,
+  //     message: message.val(),
+  //   },
+  //   success: function( xhr, response ) {
+  //     notify.success( response.message || 'Message sent, thanks!' );
+  //     e.target.reset();
+  //   },
+  //   error: function( xhr, error ) {
+  //     notify.error( error );
+  //   },
+  //   complete: function( xhr, response ) {
+  //     e.target.removeAttribute( 'disabled' );
+  //   },
+  // });
 };
 
 // add forms submit event
 const forms = document.querySelectorAll( 'form.contact-form' );
-for ( let i = 0; i < forms.length; ++i ) {
-  forms[ i ].addEventListener( 'submit', handler );
-}
+for ( let i = 0; i < forms.length; ++i ) forms[ i ].addEventListener( 'submit', handler );
+
+// if coming back from submitted page, show alert
+setTimeout( () => {
+  switch ( window.location.hash ) {
+    case '#mailsent'  : notify.success( 'Your message has been sent. I\'ll get back to you soon, thanks!' ); break;
+    case '#mailerror' : notify.success( 'Oops, there was a problem. Try sending again, or use the e-mail address.' ); break;
+  }
+}, 1000 );
+
 
