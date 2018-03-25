8a0383b4d24135992209e831ecb44a06261bc0ed
Rainner Lins <rainnerlins@gmail.com>
2018-03-24 23:20:20 -0400

    Check page hash after page loads.

--- src/setup/scroller.js
+++ src/setup/scroller.js
@@ -6,8 +6,6 @@ import Scroller from '../modules/Scroller';
 const scroller = new Scroller();
 const header = document.querySelector( '.header-wrap' );
 const bttwrap = document.querySelector( '#btt' );
-const btns = document.querySelectorAll( '[data-scroll]' );
-const hash = window.location.hash || '';
 
 // toggle some things when scrolling up
 scroller.onUp( sp => {
@@ -32,30 +30,41 @@ scroller.moreThan( 150, sp => {
 });
 
 // buttons that trigger page scroll
-for ( let i = 0; i < btns.length; ++i ) {
-  btns[ i ].addEventListener( 'click', e => {
-    e.preventDefault();
+const setupScrollButtons = () => {
+  let btns = document.querySelectorAll( '[data-scroll]' );
 
-    // where to scroll to (elm, num, etc)
-    let target = String( e.target.getAttribute( 'data-scroll' ) || '' ).trim();
-    if ( !target ) return;
+  for ( let i = 0; i < btns.length; ++i ) {
+    btns[ i ].addEventListener( 'click', e => {
+      e.preventDefault();
 
-    // jump to target
-    scroller.jumpTo( target, function( pos ) {
-      this.resetCallbacks();
-      // going to an element by id, update hash and hide header
-      if ( /^\#/.test( target ) ) {
-        window.location.hash = target;
-        header.classList.add( 'hidden' );
-      } else {
-        window.location.hash = '';
-      }
+      // where to scroll to (elm, num, etc)
+      let target = String( e.target.getAttribute( 'data-scroll' ) || '' ).trim();
+      if ( !target ) return;
+
+      // jump to target
+      scroller.jumpTo( target, function( pos ) {
+        this.resetCallbacks();
+        // going to an element by id, update hash and hide header
+        if ( /^\#/.test( target ) ) {
+          window.location.hash = target;
+          header.classList.add( 'hidden' );
+        } else {
+          window.location.hash = '';
+        }
+      });
     });
-  });
-}
+  }
+};
 
 // resume scroll position on page load
-if ( hash ) {
-  setTimeout( () => { scroller.jumpTo( hash ); }, 1000 );
-}
+window.addEventListener( 'load', e => {
+  const hash = window.location.hash || '';
+
+  if ( hash && hash !== '#' ) {
+    setTimeout( () => {
+      scroller.jumpTo( hash );
+    }, 1000 );
+  }
+  setupScrollButtons();
+});
 
