c6a61ddbd902e39ded00c55aaf7ff0d74192e7a6
Rainner Lins <rainnerlins@gmail.com>
2018-03-24 23:20:38 -0400

    Changed tooltips code to execute after page loads.

--- src/setup/tooltips.js
+++ src/setup/tooltips.js
@@ -2,10 +2,9 @@
  * Setup tooltips.
  */
 import Tooltip from '../modules/Tooltip';
-
 const tooltip = new Tooltip();
-const elms = document.querySelectorAll( '[title]' );
 
-for ( let i = 0; i < elms.length; ++i ) {
-  tooltip.select( elms[ i ] );
-}
+window.addEventListener( 'load', e => {
+  let elms = document.querySelectorAll( '[title]' );
+  for ( let i = 0; i < elms.length; ++i ) tooltip.select( elms[ i ] );
+}, false );
