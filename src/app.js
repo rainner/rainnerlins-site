bd08d66d7b75cec9e99f613ced1f47f83c2f6861
Rainner Lins <rainnerlins@gmail.com>
2018-03-24 23:16:59 -0400

    Moved spinner code to separate file.

--- src/app.js
+++ src/app.js
@@ -5,19 +5,9 @@ import './scss/app.scss';
 import './modules/Polyfills';
 import './setup/github';
 import './setup/links';
-import './setup/dropdowns';
-import './setup/tooltips';
-import './setup/scroller';
 import './setup/contact';
 import './setup/images';
-
-// on page loaded
-window.addEventListener( 'load', e => {
-  // hide spinner
-  let spinner = document.querySelector( '#spinner' );
-  spinner.style.display = 'none';
-  // show page content
-  let container = document.querySelector( '#app' );
-  container.style.opacity = 1;
-  // ...
-}, false );
+import './setup/scroller';
+import './setup/dropdowns';
+import './setup/tooltips';
+import './setup/spinner';
