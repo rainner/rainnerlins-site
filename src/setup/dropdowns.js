99bb0510b2dd939e41bcb08989665ba9a81758e0
Rainner Lins <rainnerlins@gmail.com>
2018-03-24 23:19:20 -0400

    Changed dropdowns code to execute after page loads.

--- src/setup/dropdowns.js
+++ src/setup/dropdowns.js
@@ -3,8 +3,7 @@
  */
 import Dropdown from '../modules/Dropdown';
 
-const elms = document.querySelectorAll( '.dropdown-menu' );
-
-for ( let i = 0; i < elms.length; ++i ) {
-  new Dropdown( elms[ i ] );
-}
+window.addEventListener( 'load', e => {
+  let elms = document.querySelectorAll( '.dropdown-menu' );
+  for ( let i = 0; i < elms.length; ++i ) new Dropdown( elms[ i ] );
+}, false );
